b4309886ac295be41dfda6b0fa7de07fdb988f1d
Dail Magee Jr <dailma@users.noreply.github.com>
2016-06-28 11:26:04 -0700

    Add files via upload
    Ongoing changes...

--- NinjaArena.js
+++ NinjaArena.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-	
+	$("#playerWrapper").hide();
+	$("#arenaWrapper").data("arenas",
+		{"Japanese":"url('japaneseArena.jpg')",
+		 "Rome":"url('romeArena.jpg')",
+		 "Matrix":"url('matrixArena.jpg')",
+		 "Forest":"url('forestArena.jpg')",
+		 "Snow":"url('snowArena.jpg')",
+		 "Outerspace":"url('outerspaceArena.jpg')"});
+	$("#slArena").change(function(evt){
+		var selectedArena = $(evt.target).val();
+		var arenaName = $("#arenaWrapper").data("arenas").selectedArena;
+		console.log(selectedArena, arenaName);
+		// $("div#container").css("background-image",$("#arenaWrapper."))
+	});
 });
